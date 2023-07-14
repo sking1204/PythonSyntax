4a5b6b0646ca44af13b532024f712c7dec3855fc
Shanaz King <sking1128@YewYew1128.localdomain>
2023-07-13 20:46:09 -0400

    updating file

--- sb_python/functions.py
+++ sb_python/functions.py
@@ -32,5 +32,17 @@
 #            from_email='Naz@hello.com,',
 #            body="Hi Annie, you are my cat. I love you!")
 
-def power(num,power=2):
-    return num **power
+# def power(num,power=2):
+#     return num **power
+
+def add_limited_numbers(a,b):
+    """Add two numbers, making sure sum caps at 100."""
+
+    sum = a+b
+
+    #If this required explanation, comment like this
+
+    if sum > 100:
+        sum=100
+
+    return sum
